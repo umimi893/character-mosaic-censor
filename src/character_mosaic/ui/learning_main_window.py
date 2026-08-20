ade9a5a94712883a87f7cf59214993e22266d62b
umimi893 <namahamuny@gmail.com>
2026-08-21 07:14:16 +0900

    fix: keep learning dialog lifecycle simple

--- src/character_mosaic/ui/learning_main_window.py
+++ src/character_mosaic/ui/learning_main_window.py
@@ -16,9 +16,7 @@ class LearningMainWindow(EnhancedMainWindow):
 
     def open_learning_dialog(self) -> None:
         if self._learning_dialog is None:
-            dialog = LearningDialog(self._language, self)
-            dialog.setAttribute(dialog.widgetAttribute("WA_DeleteOnClose") if False else 0, False)
-            self._learning_dialog = dialog
+            self._learning_dialog = LearningDialog(self._language, self)
         self._learning_dialog.show()
         self._learning_dialog.raise_()
         self._learning_dialog.activateWindow()
